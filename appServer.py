9c3db45de1b222a409b806a9916721d25f3964d8
Allen Richard Junge <jungal01@luther.edu>
2017-05-09 11:45:57 -0500

    added routes

---
+++ appServer.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+import sqlalchemy
+
+app = Flask(__name__)
+
+@app.route('/')
+
+@app.route('/movie')
+
+@app.route('/history', method=['GET'])
+
+@app.route('/history', method=['DELETE'])
+
+@app.route('/history/movie')
