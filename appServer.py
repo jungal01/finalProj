2ed0f0cd27a4dc8d1138e7962f7b9efc4dd7f5c5
Allen Richard Junge <jungal01@luther.edu>
2017-05-11 00:52:37 -0500

    added index.html path

--- appServer.py
+++ appServer.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import sqlalchemy
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 
@@ -8,8 +8,20 @@ engine = create_engine('sqlite:///:memory', echo=True)
 db = sessionmaker(bind=engine)
 
 @app.route('/')
+def allMovies():
+    movies = set()
+    movieIds = set()
+    for x in db.query(title):
+        movies.add((x.title, x.year))
+        movieIds.add(x.index)
+
+    return render_template('index.html',
+                            movies= list(movies),
+                            columns=[x for x in movieIds])
 
 @app.route('/movie')
+def specificMovie(movie):
+    for x in db.query()
 
 @app.route('/history', method=['GET'])
 
