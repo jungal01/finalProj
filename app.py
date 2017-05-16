e1104189f2a8f1cadb3be1bda2e3441da6a70abe
sagedb <debrsa01@luther.edu>
2017-05-15 23:30:43 -0500

    added comments to app.py

--- app.py
+++ app.py
@@ -8,32 +8,17 @@ from json import load
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://swplgjqcmrfmmy:50c0e2902aca895e575bb703e08ad518c8a08b3bb17254be94b064aa9bd134db@ec2-54-235-120-39.compute-1.amazonaws.com:5432/d9moo2fhh5flfn'
-# db = SQLAlchemy(app)
 engine = create_engine('postgres://swplgjqcmrfmmy:50c0e2902aca895e575bb703e08ad518c8a08b3bb17254be94b064aa9bd134db@ec2-54-235-120-39.compute-1.amazonaws.com:5432/d9moo2fhh5flfn')
-# Session = sessionmaker(bind=engine)
-# session = Session()
-#
-# Base = declarative_base()
 
-connection = engine.connect()
-result = connection.execute("select title, year from title")
-for row in result:
-    print("title,year:", row['title'], row['year'])
-connection.close()
-
-
-
+# Now, we can do queries using connection.execute("SQL_QUERY")
+# Use indexing to get the column value from result (see example in index())
 @app.route('/')
 @app.route('/index')
 def index():
-    # query = db.query(Title).filter_by(title='Inception').first()
-    # titles = session.query(Title).order_by(asc(Title.title)).limit(10)
     connection = engine.connect()
     result = connection.execute("select title from title limit 10")
     alist = []
     for row in result:
-        # print("title,year:", row['title'], row['year'])
         alist.append(row['title'])
     connection.close()
 
